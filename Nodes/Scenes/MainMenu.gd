2fa43f57e4ed2539bbb32c2f2ffa6917bfdbf639
AshTheWarmonger <kydrabusiness@gmail.com>
2023-11-22 15:52:47 -0500

    Added Main Menu
    -Main menu that has play button functionality and loads into the "playscreen" scene.
    -Also added a non functional lvl 2

---
+++ Nodes/Scenes/MainMenu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+
+func _on_play_button_pressed()-> void:
+	var playscreen = load("res://Nodes/Scenes/playscreen.tscn")
+	get_tree().change_scene_to_packed(playscreen)
+
+func _on_quit_button_button_up():
+	get_tree().quit()
