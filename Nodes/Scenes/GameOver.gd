00f9f68bf968ebdd2c16ec81b83cdf8549a1623b
AshTheWarmonger <kydrabusiness@gmail.com>
2023-11-27 12:42:32 -0500

    Small Update
    Added:
    -Temp restart level
    -Fixed Enemy hitbox from being tiny to more accurately sized
    -Added Barbed Wire hitbox to wall (so you can get that set up for dmg)
    -Modified player shoot speed to ".15" faster
    -Enemy movement was reduced from 500 to 350

--- Nodes/Scenes/GameOver.gd
+++ Nodes/Scenes/GameOver.gd
@@ -3,10 +3,10 @@ extends Control
 
 
 
-
 func _on_retry_button_pressed():
-	# Restarts level
-	pass 
+	#will reload current level when properly set up, for now it just refreshes the whole playscreen scene
+	get_tree().reload_current_scene()
+	get_tree().paused = false
 
 func _on_main_menu_button_pressed()-> void:
 	# Returns User to Main Menu
